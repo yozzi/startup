e2abf556565f6ba908d23a893de12098e3ba95c6
yozzi <yozz.online@gmail.com>
2015-08-12 15:35:05 -0400

    Removed some activation options

--- inc/activate.php
+++ inc/activate.php
@@ -18,32 +18,9 @@
 //$role->remove_cap( 'delete_others_posts' );
 //$role->remove_cap( 'manage_categories' );
 
-//Pour activer Menu Dynamique
-//$role->add_cap( 'edit_theme_options' );  //Activer (run once)
-//$role->remove_cap( 'edit_theme_options' ); //Désactiver (run once)
-
-//Pour activer Slider
-
-//Pour activer Images libres
-//Installer le plugin Pixabay Images
-//https://wordpress.org/plugins/pixabay-images/
-//Configurer les options : Français / 30 / Photos / All / Uncheck / Uncheck
-
 //Pour activer Commentaires
 //function startup_com(){
 //add_menu_page( 'StartUp Commentaires', 'Commentaires', 'read', 'edit-comments.php', '' ); //Activer
 //add_menu_page( 'StartUp Commentaires', 'Commentaires', 'read', 'startup-com', 'startup_com_init' ); //Désactiver
 //}
-
-//Pour activer Google Maps
-
-//Pour activer Facebook
-
-//Pour activer Twitter
-
-//Pour activer Multilingue
-
-//Pour activer Shop
-
-//Pour activer Statistiques
 ?>
