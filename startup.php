1802aac4af51b6f19b7c86973e0a169c0fd86947
yozzi <yozz.online@gmail.com>
2015-08-13 11:14:52 -0400

    Bug fix

--- startup.php
+++ startup.php
@@ -3,7 +3,7 @@
 Plugin Name: StartUp
 Description: Le plugin pour les fonctions de base de StartUp Reloaded
 Author: Yann Caplain
-Version: 1
+Version: 1.0
 */
 
 /************************** Includes */
@@ -51,16 +51,16 @@ add_action('admin_head', 'yozz_custom_admin_head_everyone');
 
 /************************** Rediriger vers une autre page au login */
 
-function yozz_login_redirect( $redirect_to, $request, $user )
-{
-if ( is_array( $user->roles ) ) {
-if ( in_array( 'owner', $user->roles ) ) {
-return admin_url( 'edit.php?post_type=page' );
-} else {
-return admin_url();
+function yozz_login_redirect( $redirect_to, $request, $user ) {
+    if ( is_array( $user->roles ) ) {
+        if ( in_array( 'owner', $user->roles ) ) {
+            return admin_url( 'edit.php?post_type=page' );
+        } else {
+            return admin_url();
+        }
+    }
 }
-}
-}
+
 add_filter( 'login_redirect', 'yozz_login_redirect', 10, 3 );
 
 /************************** Retirer des éléments du menu */
@@ -109,172 +109,189 @@ function yozz_remove_admin_menus (){
             //remove_menu_page('options-general.php'); // Settings
             remove_menu_page('profile.php'); // Profile
             remove_menu_page('wpcf7'); // Contact
+        }
+    }
 }
-}
-}
+
 add_action('admin_menu', 'yozz_remove_admin_menus');
 
 /************************** Renommer des éléments du menu */
 
 function yozz_edit_admin_menus() {
-global $menu;
-
-$menu[5][0] = 'Blog'; // Change Posts to Blog
+    global $menu;
+    $menu[5][0] = 'Blog'; // Change Posts to Blog
 }
+
 add_action( 'admin_menu', 'yozz_edit_admin_menus' );
 
 /************************** Réorganiser le menu */
 //Pour que les pages appraîssent avant les post
 
 function yozz_custom_menu_order($menu_ord) {
-if (!current_user_can('manage_options')) {
-if (!$menu_ord) return true;
-return array('edit.php?post_type=page', 'edit.php');
+    if (!current_user_can('manage_options')) {
+        if (!$menu_ord) return true;
+        return array('edit.php?post_type=page', 'edit.php');
+    }
 }
-}
+
 add_filter('custom_menu_order', 'yozz_custom_menu_order');
+
 add_filter('menu_order', 'yozz_custom_menu_order');
 
 /************************** Retirer des éléments de la barre */
 
 function yozz_remove_admin_bar_links() {
-global $wp_admin_bar, $current_user;
+    global $wp_admin_bar, $current_user;
 
-// To remove WordPress logo and related submenu items
-$wp_admin_bar->remove_menu('wp-logo');
-$wp_admin_bar->remove_menu('about');
-$wp_admin_bar->remove_menu('wporg');
-$wp_admin_bar->remove_menu('documentation');
-$wp_admin_bar->remove_menu('support-forums');
-$wp_admin_bar->remove_menu('feedback');
+    // To remove WordPress logo and related submenu items
+    $wp_admin_bar->remove_menu('wp-logo');
+    $wp_admin_bar->remove_menu('about');
+    $wp_admin_bar->remove_menu('wporg');
+    $wp_admin_bar->remove_menu('documentation');
+    $wp_admin_bar->remove_menu('support-forums');
+    $wp_admin_bar->remove_menu('feedback');
 
-// To remove Site name/View Site submenu and Edit menu from front end
-//$wp_admin_bar->remove_menu('site-name');
-$wp_admin_bar->remove_menu('view-site');
-$wp_admin_bar->remove_menu('edit');
+    // To remove Site name/View Site submenu and Edit menu from front end
+    //$wp_admin_bar->remove_menu('site-name');
+    $wp_admin_bar->remove_menu('view-site');
+    $wp_admin_bar->remove_menu('edit');
 
-// To remove Update Icon/Menu
-$wp_admin_bar->remove_menu('updates');
+    // To remove Update Icon/Menu
+    $wp_admin_bar->remove_menu('updates');
 
-// To remove Comments Icon/Menu
-$wp_admin_bar->remove_menu('comments');
+    // To remove Comments Icon/Menu
+    $wp_admin_bar->remove_menu('comments');
 
-// To remove ' New ' Menu
-$wp_admin_bar->remove_menu('new-content');
+    // To remove ' New ' Menu
+    $wp_admin_bar->remove_menu('new-content');
 
-// To remove ' Howdy,user ' Menu completely and Search field from front end
-//$wp_admin_bar->remove_menu('top-secondary');
-$wp_admin_bar->remove_menu('search'); 
+    // To remove ' Howdy,user ' Menu completely and Search field from front end
+    //$wp_admin_bar->remove_menu('top-secondary');
+    $wp_admin_bar->remove_menu('search'); 
 
-// To remove ' Howdy,user ' subMenus 
-//$wp_admin_bar->remove_menu('user-actions');
-$wp_admin_bar->remove_menu('user-info');
-//$wp_admin_bar->remove_menu('edit-profile');   
-//$wp_admin_bar->remove_menu('logout');
+    // To remove ' Howdy,user ' subMenus 
+    //$wp_admin_bar->remove_menu('user-actions');
+    $wp_admin_bar->remove_menu('user-info');
+    //$wp_admin_bar->remove_menu('edit-profile');   
+    //$wp_admin_bar->remove_menu('logout');
 
-// To remove the user details tab   
-//$wp_admin_bar->remove_menu('my-account');
+    // To remove the user details tab   
+    //$wp_admin_bar->remove_menu('my-account');
 }
+
 add_action( 'wp_before_admin_bar_render', 'yozz_remove_admin_bar_links' );
 
 /************************** Modifier le message "Howdy, " */
 
 function yozz_replace_howdy( $wp_admin_bar ) {
-$my_account=$wp_admin_bar->get_node('my-account');
-$newtitle = str_replace( 'Salutations,', '', $my_account->title );            
-$wp_admin_bar->add_node( array(
-'id' => 'my-account',
-'title' => $newtitle,
-) );
+    $my_account=$wp_admin_bar->get_node('my-account');
+    $newtitle = str_replace( 'Salutations,', 'Vous êtes connecté en tant que', $my_account->title );            
+    $wp_admin_bar->add_node(
+        array(
+            'id' => 'my-account',
+            'title' => $newtitle,
+        )
+    );
 }
+
 add_filter( 'admin_bar_menu', 'yozz_replace_howdy',25 );
 
 /************************** Retirer le panneau "Options de l'écran" */
 
 function yozz_panneau_options() {
-if ( !current_user_can( 'manage_options' ) )
-add_filter('screen_options_show_screen', '__return_false');
+    if ( !current_user_can( 'manage_options' ) )
+        add_filter('screen_options_show_screen', '__return_false');
 }
+
 add_action( 'plugins_loaded', 'yozz_panneau_options' );
 
 /************************** Désactiver le glisser déposer des boîtes */
 //Commenté car empêche le bon fonctionnement de beaucoup de choses
 
 //function yozz_disable_drag_metabox() {
-//if (!current_user_can('manage_options')) {
-//wp_deregister_script('postbox');
+//    if (!current_user_can('manage_options')) {
+//        wp_deregister_script('postbox');
+//    }
 //}
-//}
-
+//
 //add_action( 'admin_init', 'yozz_disable_drag_metabox' );
 
 /************************** Retirer le lien vers l'aide */
 
-add_filter( 'contextual_help', 'yozz_remove_help_tabs', 999, 3 );
 function yozz_remove_help_tabs($old_help, $screen_id, $screen){
-$screen->remove_help_tabs();
-return $old_help;
+    $screen->remove_help_tabs();
+    return $old_help;
 }
 
-/************************** Retirer la barre d'admin sur le site*/
+add_filter( 'contextual_help', 'yozz_remove_help_tabs', 999, 3 );
+
+/************************** Retirer la barre d'admin sur fontend */
 
 function yozz_retirer_barre() {
-if ( !current_user_can( 'manage_options' ) )
-add_filter('show_admin_bar', '__return_false');
+    add_filter('show_admin_bar', '__return_false');
 }
+
 add_action( 'plugins_loaded', 'yozz_retirer_barre' );
 
 /************************** Ajouter lien yozz.net */
 
 function yozz_admin_bar_new_item() {
-global $wp_admin_bar;
-$wp_admin_bar->add_menu(array(
-'id' => 'wp-admin-bar-startup',
-'title' => __('StartUp beta par yozz.net'),
-'href' => 'http://yozz.net'
-));
+    global $wp_admin_bar;
+    $wp_admin_bar->add_menu(
+        array(
+            'id' => 'wp-admin-bar-startup',
+            'title' => __('StartUp / yozz.net'),
+            'href' => 'http://startup.yozz.net'
+        )
+    );
 }
+
 add_action('wp_before_admin_bar_render', 'yozz_admin_bar_new_item', 10);
 
 /************************** Modifier le numéro de version */
 
 function yozz_change_footer_version() {
-return 'Version beta 0.3';
+    return 'Version 1.0';
 }
+
 add_filter( 'update_footer', 'yozz_change_footer_version', 9999 );
 
 /************************** WordPress Admin change footer text */
 
 function yozz_remove_footer_admin () {
-echo 'Vous utilisez l\'outil StartUp de <a href="http://yozz.net">yozz.net</a>';
+    echo 'Vous utilisez l\'application <a href="http://startup.yozz.net" target="_blank">StartUp</a> développée par <a href="http://yozz.net" target="_blank">yozz.net</a>';
 }
+
 add_filter('admin_footer_text', 'yozz_remove_footer_admin');
 
 /************************** Ajouter un lien All Settings pour l'admin */
 
 //function yozz_all_settings_link() {
-//add_options_page(__('All Settings'), __('Adv. Settings'), 'administrator', 'options.php');
+//    add_options_page(__('All Settings'), __('Adv. Settings'), 'administrator', 'options.php');
 //}
+//
 //add_action('admin_menu', 'yozz_all_settings_link');
 
 /************************** Modifier TinyMCE */
 
 // Utiliser l'éditeur WYSIWYG par défaut
 function yozz_default_editor() {
-if (!current_user_can('manage_options')) {
-$r = 'tinymce'; // html or tinymce
-return $r;
+    if (!current_user_can('manage_options')) {
+        $r = 'tinymce'; // html or tinymce
+        return $r;
+    }
 }
-}
+
 add_filter( 'wp_default_editor', 'yozz_default_editor' );
 
 // Utiliser l'éditeur HTML par défaut pour l'admin
 function yozz_admin_default_editor() {
-if (current_user_can('manage_options')) {
-$r = 'html'; // html or tinymce
-return $r;
+    if (current_user_can('manage_options')) {
+        $r = 'html'; // html or tinymce
+        return $r;
+    }
 }
-}
+
 add_filter( 'wp_default_editor', 'yozz_admin_default_editor' );
 
 // J'ai désactivé tout ce qui suit car ça donne un bug sous la zone d'edition du contenu
