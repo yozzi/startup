03b96b2e82ce2b88b6354b5e54a7f420af3844f5
yozz <yozz.online@gmail.com>
2015-10-16 15:26:47 -0400

    Dev

--- inc/help-content.php
+++ inc/help-content.php
@@ -1,5 +1,87 @@
 <div class='wrap'>
     <h2>StartUp Help and Reference</h2>
+    <h3>Post Types</h3>
+    <table class="widefat">
+        <thead>
+        <tr>
+            <th class="row-title">Post Type</th>
+            <th>Details</th>
+            <th>Requirement</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="row-title">Slider</td>
+            <td>manage the slides on the homepage</td>
+            <td>StartUp Slider</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Home</td>
+            <td>Manage sections for the homepage, can be used as a full block content or individually displayed by ID</td>
+            <td>StartUp Home</td>
+        </tr>
+        <tr>
+            <td class="row-title">Sections</td>
+            <td>Great way to generate content sections with image, parallax or video background and buttons to include in other content such as pages</td>
+            <td>StartUp Sections</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Blog</td>
+            <td>Shuffle or Grid style</td>
+            <td>Set style in Theme Options / Post Types</td>
+        </tr>
+        <tr>
+            <td class="row-title">Milestones</td>
+            <td>A way to communicate your goals and other numbers by infographics</td>
+            <td>StartUp Milestones</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Services</td>
+            <td>Show the services you offer woth a title, summary and icon</td>
+            <td>StartUp Services</td>
+        </tr>
+        <tr>
+            <td class="row-title">Pricing</td>
+            <td>A pricing table, full of options</td>
+            <td>StartUp Pricing</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Team</td>
+            <td>Each team member can have his detail/contact card here</td>
+            <td>StartUp Team</td>
+        </tr>
+        <tr>
+            <td class="row-title">Projects</td>
+            <td>A work in progress...</td>
+            <td>StartUp Projects</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Testimonials</td>
+            <td>Display customer testimonials in a slider way</td>
+            <td>StartUp Testimonials</td>
+        </tr>
+        <tr>
+            <td class="row-title">Products</td>
+            <td>A work in progress...</td>
+            <td>StartUp Products</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Portfolio</td>
+            <td>Show your work with two different layouts</td>
+            <td>StartUp Portfolio, set style in Theme Options / Post Types</td>
+        </tr>
+        <tr>
+            <td class="row-title">Menus</td>
+            <td>A work in progress...</td>
+            <td>StartUp Menus</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Rooms</td>
+            <td>A work in progress...</td>
+            <td>StartUp Rooms</td>
+        </tr>
+        </tbody>
+    </table>    
     <h3>Shortcodes</h3>
     <table class="widefat">
         <thead>
@@ -123,6 +205,27 @@
             <td>Shuffle or Grid style</td>
             <td>Auto-added to page with slug "blog"</td>
             <td>Set style in Theme Options / Post Types</td>
+        </tr>
+        <tr>
+            <td class="row-title">Portfolio</td>
+            <td>Portfolio</td>
+            <td>Shuffle or Grid style</td>
+            <td>Auto-added to page with slug "portfolio"</td>
+            <td>StartUp Portfolio, set style in Theme Options / Post Types</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Products</td>
+            <td>Products</td>
+            <td>Shuffle style</td>
+            <td>Auto-added to page with slug "products"</td>
+            <td>StartUp Products</td>
+        </tr>
+        <tr>
+            <td class="row-title">Projects</td>
+            <td>Projects</td>
+            <td>3 in a row</td>
+            <td>Auto-added to page with slug "projects"</td>
+            <td>StartUp Projects</td>
         </tr>
         </tbody>
     </table>
