4283cc1b89c2e0ecd438e9f8957ba78cceb1c918
yozz <yozz.online@gmail.com>
2015-10-16 13:43:43 -0400

    dev

--- inc/help-content.php
+++ inc/help-content.php
@@ -1,68 +1,129 @@
 <div class='wrap'>
     <h2>StartUp Help and Reference</h2>
-    <h3>Liste des Shortcodes</h3>
-    <ul>
-        <li>[team]</li>
-        <li>[projetcs]</li>
-        <li>[testimonials]</li>
-        <li>[products]</li>
-        <li>[portfolio]</li>
-    </ul>
-
-<br class="clear" />
-
-<table class="widefat">
-	<thead>
-	<tr>
-		<th class="row-title">Shortcode</th>
-		<th>Usage</th>
-        <th>Requirement</th>
-        <th>.container</th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td class="row-title">[home]</td>
-		<td>Displays Home Sections</td>
-        <td>StartUp Home</td>
-        <td>x</td>
-	</tr>
-	<tr class="alternate">
-		<td class="row-title">[home id="xx"]</td>
-		<td>Displays selected Home Section by ID</td>
-        <td>StartUp Home</td>
-        <td></td>
-	</tr>
+    <h3>Shortcodes</h3>
+    <table class="widefat">
+        <thead>
         <tr>
-		<td class="row-title">[section id="xx"]</td>
-		<td>Displays selected Section by ID</td>
-        <td>StartUp Sections</td>
-        <td></td>
-	</tr>
-	<tr class="alternate">
-		<td class="row-title">[blog]</td>
-		<td>Displays blog Shuffle or Grid style</td>
-        <td>Set style in Theme Options / Post Types</td>
-        <td></td>
-	</tr>
-    <tr>
-		<td class="row-title">[milestones]</td>
-		<td>Displays Milestones</td>
-        <td>StartUp Milestones</td>
-        <td></td>
-	</tr>
-    	<tr class="alternate">
-		<td class="row-title">[services]</td>
-		<td>Displays Services</td>
-        <td>StartUp Services</td>
-        <td></td>
-	</tr>
-    <tr>
-		<td class="row-title">[pricing]</td>
-		<td>Displays Pricing</td>
-        <td>StartUp Pricing</td>
-        <td></td>
-	</tr>
-	</tbody>
-</table>
+            <th class="row-title">Shortcode</th>
+            <th>Displays</th>
+            <th>Desktop layout</th>
+            <th style="text-align:center">.container</th>
+            <th>Requirement</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="row-title">[home]</td>
+            <td>Home Sections</td>
+            <td>1 in first row, then 2 in a row</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Home</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[home id="xx"]</td>
+            <td>Home Section by ID</td>
+            <td></td>
+            <td style="text-align:center"></td>
+            <td>StartUp Home</td>
+        </tr>
+        <tr>
+            <td class="row-title">[section id="xx"]</td>
+            <td>Section by ID</td>
+            <td></td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Sections</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[blog]</td>
+            <td>Blog</td>
+            <td>Shuffle or Grid style</td>
+            <td style="text-align:center"></td>
+            <td>Set style in Theme Options / Post Types</td>
+        </tr>
+        <tr>
+            <td class="row-title">[milestones]</td>
+            <td>Milestones</td>
+            <td>4 in a row</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Milestones</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[services]</td>
+            <td>Services</td>
+            <td>3 in a row</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Services</td>
+        </tr>
+        <tr>
+            <td class="row-title">[pricing]</td>
+            <td>Pricing</td>
+            <td>3 in a row</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Pricing</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[team]</td>
+            <td>Team</td>
+            <td>4 in a row and in a carousel if more</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Team</td>
+        </tr>
+        <tr>
+            <td class="row-title">[projects]</td>
+            <td>Projects</td>
+            <td>3 in a row</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Projects</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[testimonials]</td>
+            <td>Testimonials</td>
+            <td>1 by 1 in a carousel</td>
+            <td style="text-align:center"><span class="dashicons dashicons-yes"></span></td>
+            <td>StartUp Testimonials</td>
+        </tr>
+        <tr>
+            <td class="row-title">[products]</td>
+            <td>Products</td>
+            <td>Shuffle style</td>
+            <td style="text-align:center"></td>
+            <td>StartUp Products</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">[portfolio]</td>
+            <td>Portfolio</td>
+            <td>Shuffle or Grid style</td>
+            <td style="text-align:center"></td>
+            <td>StartUp Portfolio, set style in Theme Options / Post Types</td>
+        </tr>
+        </tbody>
+    </table>
+    <h3>Page Templates</h3>
+    <table class="widefat">
+        <thead>
+        <tr>
+            <th class="row-title">Template</th>
+            <th>Displays</th>
+            <th>Desktop layout</th>
+            <th>Detail</th>
+            <th>Requirement</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="row-title">Home</td>
+            <td>Home Sections</td>
+            <td>1 in first row, then 2 in a row</td>
+            <td></td>
+            <td>StartUp Home</td>
+        </tr>
+        <tr class="alternate">
+            <td class="row-title">Blog</td>
+            <td>Blog</td>
+            <td>Shuffle or Grid style</td>
+            <td>Auto-added to page with slug "blog"</td>
+            <td>Set style in Theme Options / Post Types</td>
+        </tr>
+        </tbody>
+    </table>
 </div>
