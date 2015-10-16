db2c133576c493fcc38194b21b00144e9cc97dfe
yozz <yozz.online@gmail.com>
2015-10-16 11:02:51 -0400

    Help page

---
+++ inc/help-content.php
@@ -0,0 +1,68 @@
+<div class='wrap'>
+    <h2>StartUp Help and Reference</h2>
+    <h3>Liste des Shortcodes</h3>
+    <ul>
+        <li>[team]</li>
+        <li>[projetcs]</li>
+        <li>[testimonials]</li>
+        <li>[products]</li>
+        <li>[portfolio]</li>
+    </ul>
+
+<br class="clear" />
+
+<table class="widefat">
+	<thead>
+	<tr>
+		<th class="row-title">Shortcode</th>
+		<th>Usage</th>
+        <th>Requirement</th>
+        <th>.container</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td class="row-title">[home]</td>
+		<td>Displays Home Sections</td>
+        <td>StartUp Home</td>
+        <td>x</td>
+	</tr>
+	<tr class="alternate">
+		<td class="row-title">[home id="xx"]</td>
+		<td>Displays selected Home Section by ID</td>
+        <td>StartUp Home</td>
+        <td></td>
+	</tr>
+        <tr>
+		<td class="row-title">[section id="xx"]</td>
+		<td>Displays selected Section by ID</td>
+        <td>StartUp Sections</td>
+        <td></td>
+	</tr>
+	<tr class="alternate">
+		<td class="row-title">[blog]</td>
+		<td>Displays blog Shuffle or Grid style</td>
+        <td>Set style in Theme Options / Post Types</td>
+        <td></td>
+	</tr>
+    <tr>
+		<td class="row-title">[milestones]</td>
+		<td>Displays Milestones</td>
+        <td>StartUp Milestones</td>
+        <td></td>
+	</tr>
+    	<tr class="alternate">
+		<td class="row-title">[services]</td>
+		<td>Displays Services</td>
+        <td>StartUp Services</td>
+        <td></td>
+	</tr>
+    <tr>
+		<td class="row-title">[pricing]</td>
+		<td>Displays Pricing</td>
+        <td>StartUp Pricing</td>
+        <td></td>
+	</tr>
+	</tbody>
+</table>
+</div>
