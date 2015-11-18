a9caa6a008500681472f95ffd95bfc18691ff2a9
yozz <yozz.online@gmail.com>
2015-11-18 16:50:54 -0500

    Moved css to plugin

--- startup.php
+++ startup.php
@@ -51,7 +51,7 @@ add_action('admin_head', 'startup_reloaded_includes');
 if (!is_plugin_active('theme-my-login/theme-my-login.php')){
     function startup_reloaded_custom_login_style() {
         echo '<style type="text/css">';
-        include 'css/startup_admin_login.css';
+        include 'css/startup_login.css';
         echo '</style>';
     }
 
@@ -439,4 +439,66 @@ add_shortcode( 'blog', 'startup_reloaded_blog_shortcode' );
 //Remove emoji
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+/************************** Simply Show IDs 1.3.3 */
+// Prepend the new column to the columns array
+function ssid_column($cols) {
+	$cols['ssid'] = 'ID';
+	return $cols;
+}
+
+// Echo the ID for the new column
+function ssid_value($column_name, $id) {
+	if ($column_name == 'ssid')
+		echo $id;
+}
+
+function ssid_return_value($value, $column_name, $id) {
+	if ($column_name == 'ssid')
+		$value = $id;
+	return $value;
+}
+
+// Output CSS for width of new column
+function ssid_css() {
+?>
+<style type="text/css">
+	#ssid { width: 50px; } /* Simply Show IDs */
+</style>
+<?php	
+}
+
+// Actions/Filters for various tables and the css output
+function ssid_add() {
+	add_action('admin_head', 'ssid_css');
+
+	add_filter('manage_posts_columns', 'ssid_column');
+	add_action('manage_posts_custom_column', 'ssid_value', 10, 2);
+
+	add_filter('manage_pages_columns', 'ssid_column');
+	add_action('manage_pages_custom_column', 'ssid_value', 10, 2);
+
+	add_filter('manage_media_columns', 'ssid_column');
+	add_action('manage_media_custom_column', 'ssid_value', 10, 2);
+
+	add_filter('manage_link-manager_columns', 'ssid_column');
+	add_action('manage_link_custom_column', 'ssid_value', 10, 2);
+
+	add_action('manage_edit-link-categories_columns', 'ssid_column');
+	add_filter('manage_link_categories_custom_column', 'ssid_return_value', 10, 3);
+
+	foreach ( get_taxonomies() as $taxonomy ) {
+		add_action("manage_edit-${taxonomy}_columns", 'ssid_column');			
+		add_filter("manage_${taxonomy}_custom_column", 'ssid_return_value', 10, 3);
+	}
+
+	add_action('manage_users_columns', 'ssid_column');
+	add_filter('manage_users_custom_column', 'ssid_return_value', 10, 3);
+
+	add_action('manage_edit-comments_columns', 'ssid_column');
+	add_action('manage_comments_custom_column', 'ssid_value', 10, 2);
+}
+
+add_action('admin_init', 'ssid_add');
+
 ?>
