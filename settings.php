4bf2fd9e05ca8833896a656ee36f0555aadf9989
yozz <yozz.online@gmail.com>
2016-02-09 15:46:31 -0500

    Minor

--- settings.php
+++ settings.php
@@ -11,4 +11,3 @@ $product_url = 'http://startup.yozz.net';
 $startup_version = "1.0";
 
 $startup_footer = "Vous utilisez l'application <a href=\"http://startup.yozz.net\" target=\"_blank\">StartUp</a> développée par <a href=\"http://yozz.net\" target=\"_blank\">yozz.net</a>";
-
