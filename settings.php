aa7b696ea15650af959a22aa92281da4895eb8c0
yozz <yozz.online@gmail.com>
2016-02-12 11:56:53 -0500

    Dev

--- settings.php
+++ settings.php
@@ -11,3 +11,228 @@ $product_url = 'http://startup.yozz.net'; //Your product URL (string)
 $startup_version = "1.0"; //Version number (string)
 
 $startup_footer = "Vous utilisez l'application <a href=\"http://startup.yozz.net\" target=\"_blank\">StartUp</a> développée par <a href=\"http://yozz.net\" target=\"_blank\">yozz.net</a>"; //Footer text (string)
+
+/**
+ * CMB2 Theme Options
+ * @version 0.1.0
+ */
+class startup_Admin {
+
+	/**
+ 	 * Option key, and option page slug
+ 	 * @var string
+ 	 */
+	private $key = 'startup_options';
+
+	/**
+ 	 * Options page metabox id
+ 	 * @var string
+ 	 */
+	private $metabox_id = 'startup_option_metabox';
+
+	/**
+	 * Options Page title
+	 * @var string
+	 */
+	protected $title = '';
+
+	/**
+	 * Options Page hook
+	 * @var string
+	 */
+	protected $options_page = '';
+
+	/**
+	 * Holds an instance of the object
+	 *
+	 * @var startup_Admin
+	 **/
+	private static $instance = null;
+
+	/**
+	 * Constructor
+	 * @since 0.1.0
+	 */
+	private function __construct() {
+		// Set our title
+		$this->title = __( 'StartUp', 'startup' );
+	}
+
+	/**
+	 * Returns the running object
+	 *
+	 * @return startup_Admin
+	 **/
+	public static function get_instance() {
+		if( is_null( self::$instance ) ) {
+			self::$instance = new startup_Admin();
+			self::$instance->hooks();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Initiate our hooks
+	 * @since 0.1.0
+	 */
+	public function hooks() {
+		add_action( 'admin_init', array( $this, 'init' ) );
+		add_action( 'admin_menu', array( $this, 'add_options_page' ) );
+		add_action( 'cmb2_admin_init', array( $this, 'add_options_page_metabox' ) );
+	}
+
+
+	/**
+	 * Register our setting to WP
+	 * @since  0.1.0
+	 */
+	public function init() {
+		register_setting( $this->key, $this->key );
+	}
+
+	/**
+	 * Add menu options page
+	 * @since 0.1.0
+	 */
+	public function add_options_page() {
+		$this->options_page = add_options_page( $this->title, $this->title, 'manage_options', $this->key, array( $this, 'admin_page_display' ) );
+
+		// Include CMB CSS in the head to avoid FOUC
+		add_action( "admin_print_styles-{$this->options_page}", array( 'CMB2_hookup', 'enqueue_cmb_css' ) );
+	}
+
+	/**
+	 * Admin page markup. Mostly handled by CMB2
+	 * @since  0.1.0
+	 */
+	public function admin_page_display() {
+		?>
+		<div class="wrap cmb2-options-page <?php echo $this->key; ?>">
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+			<?php cmb2_metabox_form( $this->metabox_id, $this->key ); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Add the options metabox to the array of metaboxes
+	 * @since  0.1.0
+	 */
+	function add_options_page_metabox() {
+
+		// hook in our save notices
+		add_action( "cmb2_save_options-page_fields_{$this->metabox_id}", array( $this, 'settings_notices' ), 10, 2 );
+
+		$cmb = new_cmb2_box( array(
+			'id'         => $this->metabox_id,
+			'hookup'     => false,
+			'cmb_styles' => false,
+			'show_on'    => array(
+				// These are important, don't remove
+				'key'   => 'options-page',
+				'value' => array( $this->key, )
+			),
+		) );
+
+		// Set our CMB2 fields
+        
+        $cmb->add_field( array(
+			'desc' => __( 'Use wall', 'startup' ),
+			'id'   => 'wall',
+			'type' => 'checkbox',
+			'default' => true,
+		) );
+        
+        $cmb->add_field( array(
+			'desc' => __( 'Show help page', 'startup' ),
+			'id'   => 'help',
+			'type' => 'checkbox',
+			'default' => true,
+		) );
+
+		$cmb->add_field( array(
+			'desc' => __( 'Product name', 'startup' ),
+			'id'   => 'product_name',
+			'type' => 'text',
+			'default' => 'StartUp / yozz.net',
+		) );
+        
+        $cmb->add_field( array(
+			'desc' => __( 'Product url', 'startup' ),
+			'id'   => 'product_url',
+			'type' => 'text',
+			'default' => 'http://startup.yozz.net',
+		) );
+        
+        $cmb->add_field( array(
+			'desc' => __( 'Product version', 'startup' ),
+			'id'   => 'product_version',
+			'type' => 'text',
+			'default' => '1.0',
+		) );
+        
+        $cmb->add_field( array(
+			'desc' => __( 'Footer', 'startup' ),
+			'id'   => 'footer',
+			'type' => 'text',
+			'default' => 'Vous utilisez l\'application <a href="http://startup.yozz.net" target="_blank">StartUp</a> développée par <a href="http://yozz.net" target="_blank">yozz.net</a>',
+		) );
+
+
+	}
+
+	/**
+	 * Register settings notices for display
+	 *
+	 * @since  0.1.0
+	 * @param  int   $object_id Option key
+	 * @param  array $updated   Array of updated fields
+	 * @return void
+	 */
+	public function settings_notices( $object_id, $updated ) {
+		if ( $object_id !== $this->key || empty( $updated ) ) {
+			return;
+		}
+
+		add_settings_error( $this->key . '-notices', '', __( 'Settings updated.', 'startup' ), 'updated' );
+		settings_errors( $this->key . '-notices' );
+	}
+
+	/**
+	 * Public getter method for retrieving protected/private variables
+	 * @since  0.1.0
+	 * @param  string  $field Field to retrieve
+	 * @return mixed          Field value or exception is thrown
+	 */
+	public function __get( $field ) {
+		// Allowed fields to retrieve
+		if ( in_array( $field, array( 'key', 'metabox_id', 'title', 'options_page' ), true ) ) {
+			return $this->{$field};
+		}
+
+		throw new Exception( 'Invalid property: ' . $field );
+	}
+
+}
+
+/**
+ * Helper function to get/return the startup_Admin object
+ * @since  0.1.0
+ * @return startup_Admin object
+ */
+function startup_admin() {
+	return startup_Admin::get_instance();
+}
+
+/**
+ * Wrapper function around cmb2_get_option
+ * @since  0.1.0
+ * @param  string  $key Options array key
+ * @return mixed        Option value
+ */
+function startup_get_option( $key = '' ) {
+	return cmb2_get_option( startup_admin()->key, $key );
+}
+
+// Get it started
+startup_admin();
