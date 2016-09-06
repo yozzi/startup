42f6315c59c7ac9c1505a270932dd0a3ece78e3e
yozz <yozz.online@gmail.com>
2016-09-06 16:29:21 -0400

    Slug admin column

--- startup.php
+++ startup.php
@@ -60,4 +60,57 @@ require('inc/conf.php');
 require('inc/help.php');
 
 require('inc/wall.php');
+
+// Slug columns
+Class WPAdminSlugColumn {
+
+	/**
+	* This is the constructor for WPAdminSlugColumn Class
+	*
+	* @return void
+	*/
+	public function __construct() {
+		add_filter( 'manage_posts_columns', array( $this, 'SAC_posts' ) );
+		add_action( 'manage_posts_custom_column', array( $this, 'SAC_posts_data' ), 10, 2);
+		add_filter( 'manage_pages_columns', array( $this, 'SAC_pages' ) );
+		add_action( 'manage_pages_custom_column', array( $this, 'SAC_pages_data' ), 10, 2);
+	}
+
+	/**
+	* Adds slug to Posts column with option
+	*
+	* @return void
+	*/
+	public function SAC_posts( $defaults ) {
+		$defaults['SAC_Slug'] = __( 'Post Slug' );
+		return $defaults;
+	}
+
+	public function SAC_posts_data( $column_name, $id ) {
+		if ( $column_name == 'SAC_Slug' ) {
+			$post_slug = get_post( $id )->post_name;
+			echo $post_slug;
+		}
+	}
+
+	/**
+	* Adds slug to Pages column with option
+	*
+	* @return void
+	*/
+	public function SAC_pages( $defaults ) {
+		$defaults['SAC_Slug'] = __( 'Page Slug' );
+		return $defaults;
+	}
+
+	public function SAC_pages_data( $column_name, $id ) {
+		if ( $column_name == 'SAC_Slug' ) {
+			$page_slug = get_page( $id )->post_name;
+			echo $page_slug;
+		}
+	}
+
+}
+
+$WPAdminSlugColumn = new WPAdminSlugColumn();
 ?>
