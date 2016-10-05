b87bda686b25d98c6d20a9ebca044c49ed77d861
yozz <yozz.online@gmail.com>
2016-10-05 13:42:10 -0400

    Add a all-shortcodes-admin-page

--- inc/menu.php
+++ inc/menu.php
@@ -126,3 +126,76 @@ add_action('admin_menu', 'startup_post_remove');   //adding action for triggerin
 //add_filter('custom_menu_order', 'startup_custom_menu_order');
 
 //add_filter('menu_order', 'startup_custom_menu_order');
+
+
+
+
+
+/************************** Ajouter une page avec la liste de tous les shortcodes */
+if(is_admin())
+{
+	// Create the Paulund toolbar
+	$shortcodes = new View_All_Available_Shortcodes();
+}
+
+/**
+ * View all available shrotcodes on an admin page
+ *
+ * @author
+ **/
+class View_All_Available_Shortcodes
+{
+	public function __construct()
+	{
+		$this->Admin();
+	}
+	/**
+	 * Create the admin area
+	 */
+	public function Admin(){
+		add_action( 'admin_menu', array(&$this,'Admin_Menu') );
+	}
+
+	/**
+	 * Function for the admin menu to create a menu item in the settings tree
+	 */
+	public function Admin_Menu(){
+		add_submenu_page(
+			'options-general.php',
+			'View All Shortcodes',
+			'View All Shortcodes',
+			'manage_options',
+			'view-all-shortcodes',
+			array(&$this,'Display_Admin_Page'));
+	}
+
+	/**
+	 * Display the admin page
+	 */
+	public function Display_Admin_Page(){
+		global $shortcode_tags;
+
+        ?>
+        <div class="wrap">
+        	<div id="icon-options-general" class="icon32"><br></div>
+			<h2>View All Available Shortcodes</h2>
+			<div class="section panel">
+				<p>This page will display all of the available shortcodes that you can use on your WordPress blog.</p>
+        	<table class="widefat importers">
+        		<tr><td><strong>Shortcodes</strong></td></tr>
+        <?php
+
+	        foreach($shortcode_tags as $code => $function)
+	        {
+	        	?>
+	        		<tr><td>[<?php echo $code; ?>]</td></tr>
+	        	<?php
+	        }
+	    ?>
+
+			</table>
+			</div>
+		</div>
+		<?php
+	}
+} // END class View_All_Available_Shortcodes
