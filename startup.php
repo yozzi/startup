3040915be6a3fda7d124b91245e6509686c96a92
yozz <yozz.online@gmail.com>
2016-10-06 10:10:47 -0400

    Dashboard cusomization

--- startup.php
+++ startup.php
@@ -31,6 +31,8 @@ include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 require('inc/settings.php');
 
+require('inc/dashboard.php');
+
 require('inc/scheme.php');
 
 require('inc/cleaner.php');
